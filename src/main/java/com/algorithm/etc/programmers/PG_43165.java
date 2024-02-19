4fa0c362e351dfb27c6568791bc9c23650b9f99a
hojoon90 <hojoon90@gmail.com>
2024-02-19 23:07:08 +0900

    practice

---
+++ src/main/java/com/algorithm/etc/programmers/PG_43165.java
@@ -0,0 +1,39 @@
+package com.algorithm.etc.programmers;
+
+public class PG_43165 {
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 5;
+
+        solution(numbers, target);
+    }
+
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        dfs(numbers, 0, target, 0);
+        answer = count;
+
+        return answer;
+    }
+
+    public static void dfs(int[] numbers, int depth, int target, int result){
+        if(depth == numbers.length){
+            if (target == result) {
+                count++;
+            }
+            return;
+        }
+
+        int plus = result + numbers[depth];
+        int minus = result - numbers[depth];
+
+        dfs(numbers, depth+1, target, plus);
+        dfs(numbers, depth+1, target, minus);
+    }
+}
