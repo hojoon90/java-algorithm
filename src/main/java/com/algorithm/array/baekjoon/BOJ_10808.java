234f8d4f1f620731213e9577a6235d0b09abcf5e
hojoon90 <hojoon90@gmail.com>
2024-08-13 18:32:31 +0900

    array

---
+++ src/main/java/com/algorithm/array/baekjoon/BOJ_10808.java
@@ -0,0 +1,27 @@
+package com.algorithm.array.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_10808 {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] countArr = new int[26];
+        String[] word = br.readLine().split("");
+
+        for(String alphabet: word){
+            int ascii = alphabet.charAt(0);
+            int idx = ascii - 'a';
+
+            countArr[idx] += 1;
+        }
+
+        for(int i: countArr){
+            System.out.print(i+ " ");
+        }
+
+    }
+}
