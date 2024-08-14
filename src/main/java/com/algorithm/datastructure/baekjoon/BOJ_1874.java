3561a415cb63052931de458a6cf1ba13cb5bf56c
hojoon90 <hojoon90@gmail.com>
2024-08-14 18:47:07 +0900

    linkedList stack

--- src/main/java/com/algorithm/datastructure/baekjoon/BOJ_1874.java
+++ src/main/java/com/algorithm/datastructure/baekjoon/BOJ_1874.java
@@ -3,9 +3,8 @@ package com.algorithm.datastructure.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
-public class BOJ_1834 {
+public class BOJ_1874 {
 
 
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/com/algorithm/datastructure/baekjoon/BOJ_2493.java
@@ -0,0 +1,35 @@
+package com.algorithm.datastructure.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2493 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        int[] arr = new int[count];
+        String[] s = br.readLine().split(" ");
+        for (int i = 0; i < s.length; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i = 0; i < count; i++){
+            while(!stack.isEmpty()){
+                if(stack.peek()[1] >= arr[i]){
+                    System.out.print(stack.peek()[0] + 1 +" ");
+                    break;
+                }
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                System.out.print("0 ");
+            }
+
+            stack.push(new int[] {i, arr[i]});
+        }
+    }
+}
