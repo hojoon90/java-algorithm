2c2da1f6316fc06da0765718c271b3c6e49ed38a
hojoon90 <hojoon90@gmail.com>
2024-08-15 17:52:47 +0900

    queue

---
+++ src/main/java/com/algorithm/datastructure/baekjoon/BOJ_2164.java
@@ -0,0 +1,25 @@
+package com.algorithm.datastructure.baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2164 {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= count; i++){
+            queue.offer(i);
+        }
+
+        while(queue.size() != 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.peek());
+    }
+
+}
